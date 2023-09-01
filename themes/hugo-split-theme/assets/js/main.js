5b9f4d14b68e31657f60f8929c5e0c9240ba9e3d
Louis-Lefebvre <louislefebvre1999@gmail.com>
2023-09-01 14:28:22 -0500

    fix: photo cache and rm blog for now

--- themes/hugo-split-theme/assets/js/main.js
+++ themes/hugo-split-theme/assets/js/main.js
@@ -2,8 +2,22 @@ import * as params from '@params';
 
 let imgs = params.Album
 function scrollImages(event) {
-    event.target.style.backgroundImage = "url(/image/"+imgs[Math.floor(Math.random() * imgs.length)]+")";
+    let targetImage = imgs[Math.floor(Math.random() * imgs.length)];
+    event.target.style.backgroundImage = "url(/image/"+targetImage.name+")";
+    if (targetImage.position) {
+        event.target.style.backgroundPosition = targetImage.position;
+    } else {
+        event.target.style.backgroundPosition = "center";
+    }
 }
 
 const imageDiv = document.getElementsByClassName('split-image')[0];
 imageDiv.onclick = scrollImages;
+
+// Cache all the images on the page in the background
+imgs.forEach((element) => {
+    var images = new Array();
+    var i = new Image();
+    i.src = element;
+    images.push(i);
+});
