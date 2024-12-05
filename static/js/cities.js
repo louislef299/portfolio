27f4588066b3d40ae294f2beb4690d7c7cf2c179
Louis Lefebvre <lefebl4@medtronic.com>
2024-12-05 15:40:21 -0600

    feat: Simple travel map

---
+++ static/js/cities.js
@@ -0,0 +1,6 @@
+CITIES =
+[
+   {name: 'Bozeman', latitude: 45.6770, longitude: -111.0429, radius: 3, fillKey: 'city', date: '2023-01'},
+   {name: 'Santa Cruz', latitude: 36.9741, longitude: -122.0308, radius: 3, fillKey: 'city', date: '2023-01'},
+   {name: 'New York City', latitude: 40.7128, longitude: -74.0060, radius: 3, fillKey: 'city', date: '2023-01'},
+]
