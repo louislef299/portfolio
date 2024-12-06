77e2f0bbfbc894b9d29ce639139413cec0e54129
louislef299 <louislef299@users.noreply.github.com>
2024-12-06 01:34:09 +0000

    deploy: 3073ee781dffd0fa99e54cf44b0871a100231b2a

---
+++ js/cities.js
@@ -0,0 +1,23 @@
+CITIES =
+[
+   {name: 'Bozeman', latitude: 45.6770, longitude: -111.0429, radius: 3, fillKey: 'city', date: '2017-03'},
+   {name: 'Santa Cruz', latitude: 36.9741, longitude: -122.0308, radius: 3, fillKey: 'city', date: '2023-11'},
+   {name: 'New York City', latitude: 40.7128, longitude: -74.0060, radius: 3, fillKey: 'city', date: '2023-01'},
+   {name: 'Stoney Point', latitude: 46.92551345416876, longitude: -91.82165598465556, radius: 3, fillKey: 'city', date: '2024-11'},
+   {name: 'San Diego', latitude: 32.7157, longitude: -117.1611, radius: 3, fillKey: 'city', date: '2022-07'},
+   {name: 'Washington, DC', latitude: 38.9072, longitude: -77.0369, radius: 3, fillKey: 'city', date: '2023-01'},
+   {name: 'Seattle', latitude: 47.6062, longitude: -122.3321, radius: 3, fillKey: 'city', date: '2020-08'},
+   {name: 'Daytona Beach', latitude: 29.2108, longitude: -81.0228, radius: 3, fillKey: 'city', date: '2017-03'},
+   {name: 'Whitefish', latitude: 48.4117, longitude: -114.3350, radius: 3, fillKey: 'city', date: '2018-08'},
+   {name: 'Cloud Peak', latitude: 44.3844, longitude: -107.1739, radius: 3, fillKey: 'city', date: '2023-01'},
+   {name: 'Denver', latitude: 39.7392, longitude: -104.9903, radius: 3, fillKey: 'city', date: '2022-02'},
+   {name: 'Cape Coral', latitude: 26.5629, longitude: -81.9495, radius: 3, fillKey: 'city', date: '2021-04'},
+   {name: 'Cancun', latitude: 21.1619, longitude: -86.8515, radius: 3, fillKey: 'city', date: '2023-03'},
+   {name: 'St. Thomas, Bakkero', latitude: 18.3381, longitude: -64.9407, radius: 3, fillKey: 'city', date: '2014-04'},
+   {name: 'San Jose', latitude: 9.9281, longitude: -84.0907, radius: 3, fillKey: 'city', date: '2015-04'},
+   {name: 'London', latitude: 51.5074, longitude: -0.1278, radius: 3, fillKey: 'city', date: '2019-11'},
+   {name: 'Montpellier', latitude: 43.6119, longitude: 3.8772, radius: 3, fillKey: 'city', date: '2019-09'},
+   {name: 'Lagos', latitude: 37.1020, longitude: -8.6742, radius: 3, fillKey: 'city', date: '2022-03'},
+   {name: 'Barcelona', latitude: 41.3851, longitude: 2.1734, radius: 3, fillKey: 'city', date: '2023-11'},
+   {name: 'Emo', latitude: 49.003179617088044, longitude: -93.71126630277345, radius: 3, fillKey: 'city', date: '2023-07'}
+]
---
+++ js/countries.js
@@ -0,0 +1,18 @@
+// Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+COUNTRIES =
+{
+    MEX: {fillKey: 'visited'},
+    USA: {fillKey: 'visited'},
+    FRA: {fillKey: 'visited'},
+    ESP: {fillKey: 'visited'},
+    MCO: {fillKey: 'visited'},
+    ITA: {fillKey: 'visited'},
+    CRI: {fillKey: 'visited'},
+    VGB: {fillKey: 'visited'},
+    CYM: {fillKey: 'visited'},
+    CHE: {fillKey: 'visited'},
+    GBR: {fillKey: 'visited'},
+    CAN: {fillKey: 'visited'},
+    NLD: {fillKey: 'visited'},
+    PRT: {fillKey: 'visited'},
+}
