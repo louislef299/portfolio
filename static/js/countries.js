1b96974a14eecdbf423b130c60be8803d038d3ef
Louis Lefebvre <lefebl4@medtronic.com>
2024-12-05 19:29:47 -0600

    fix: Update points on map

--- static/js/countries.js
+++ static/js/countries.js
@@ -1,7 +1,18 @@
 // Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 COUNTRIES =
 {
-    ITA: {fillKey: 'visited'},
+    MEX: {fillKey: 'visited'},
     USA: {fillKey: 'visited'},
-    THA: {fillKey: 'visited'},
+    FRA: {fillKey: 'visited'},
+    ESP: {fillKey: 'visited'},
+    MCO: {fillKey: 'visited'},
+    ITA: {fillKey: 'visited'},
+    CRI: {fillKey: 'visited'},
+    VGB: {fillKey: 'visited'},
+    CYM: {fillKey: 'visited'},
+    CHE: {fillKey: 'visited'},
+    GBR: {fillKey: 'visited'},
+    CAN: {fillKey: 'visited'},
+    NLD: {fillKey: 'visited'},
+    PRT: {fillKey: 'visited'},
 }
